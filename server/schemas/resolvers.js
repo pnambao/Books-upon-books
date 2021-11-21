a4268c16892a81d92d095bdb8a84a3923c68a39a
RoachCoach <panjenambao@gmail.com>
2021-11-20 18:30:47 -0600

    added a query and mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,6 +2,29 @@ const {Book, User} = require('../models');
 
 const resolvers = {
     Query: {
-        
+        me: async () => {
+            return await User.findbyId(args.id).populate('savedBooks')
+        }
+    },
+
+    Mutation: {
+        addUser: async (parent, {username, email, password}) =>{
+            return await User.create({username, email, password});
+        },
+
+        saveBook: async() => {
+
+        },
+
+        removeBook: async() =>{
+
+        },
+
+        login: async () => {
+
+        }
+
     }
 }
+
+module.exports = resolvers;
