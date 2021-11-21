607f496efdd87bc917ff0e21e01b4cdc526c0fc5
RoachCoach <panjenambao@gmail.com>
2021-11-21 02:05:40 -0600

    im stuck on how to do the mutation. this sucks

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
-import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+// import { saveBook, searchGoogleBooks } from '../utils/API';
+// import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -53,17 +54,20 @@ const SearchBooks = () => {
   };
 
   // create function to handle saving a book to our database
+
+
+
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
-    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    // const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    // // get token
+    // const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
-
+    // if (!token) {
+    //   return false;
+    // }
+    
     try {
       const response = await saveBook(bookToSave, token);
 
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,26 +1,82 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-mutation loginUser {
-    
-
+mutation loginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+        token
+        user{
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                title
+                description
+                authors
+                image
+                link
+            }
+        }
+    }
 }
 `; 
 
 export const ADD_USER = gql`
-mutation addUser {
-
+mutation addUser($username: String!, $email: String!, $password: String!) {
+ addUser(username: $username, email: $email, password: $password) {
+     token
+     user{
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                title
+                description
+                authors
+                image
+                link
+            }
+        }    
+     }
 }
 `; 
 
 export const SAVE_BOOK = gql`
-mutation saveUser {
-
+mutation saveBook( $input: bookInput) {
+ saveBook(input: $input){
+     _id
+     username
+     email
+     savedBooks{
+         bookId
+         authors
+         image
+         description
+         title
+         link
+        }
+    }
 }
 `; 
 
 export const REMOVE_BOOK = gql`
-mutation removeUser {
-
+mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks{
+            bookId
+            authors
+            image
+            description
+            title
+            link
+        }
+    }
 }
 `; 
