cd186990849e51fc822e25ca28ba3ad0896ec11d
RoachCoach <panjenambao@gmail.com>
2021-11-21 00:06:40 -0600

    working on client side now

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+mutation loginUser {
+    
+
+}
+`; 
+
+export const ADD_USER = gql`
+mutation addUser {
+
+}
+`; 
+
+export const SAVE_BOOK = gql`
+mutation saveUser {
+
+}
+`; 
+
+export const REMOVE_BOOK = gql`
+mutation removeUser {
+
+}
+`; 
