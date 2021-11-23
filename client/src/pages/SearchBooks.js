22812df4d27cdd0c2fe630e2284052452ee031bf
RoachCoach <panjenambao@gmail.com>
2021-11-22 23:11:05 -0600

    just run npm i

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 import Auth from '../utils/auth';
-// import { saveBook, searchGoogleBooks } from '../utils/API';
-// import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { saveBook, searchGoogleBooks } from '../utils/API';
+import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations';
 
@@ -58,16 +58,16 @@ const SearchBooks = () => {
 
 
   const handleSaveBook = async (bookId) => {
-    // find the book in `searchedBooks` state by the matching id
-    // const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    //find the book in `searchedBooks` state by the matching id
+    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-    // // get token
-    // const token = Auth.loggedIn() ? Auth.getToken() : null;
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    // if (!token) {
-    //   return false;
-    // }
-    const [saveBook, { error }] = useMutation(SAVE_BOOK);
+    if (!token) {
+      return false;
+    }
+    // const [saveBook, { error }] = useMutation(SAVE_BOOK);
     
     try {
       const response = await saveBook(bookToSave, token);
